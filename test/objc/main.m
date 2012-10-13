df876bb4857262efdef6bbb952c861fe8993160d
Baluta Cristian <cristi.baluta@gmail.com>
2012-10-13 09:13:30 +0100

    moved the generation of main.m in a subdirectory

--- test/objc/main.m
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "@Main.h"
-
-int main(int argc, char *argv[]) {
-	@autoreleasepool {
-		return UIApplicationMain(argc, argv, nil, NSStringFromClass([@Main class]));
-	}
-}
